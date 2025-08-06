6aac3adde75d77807c92c1bf3d0b6a66ff37289a
kavi-Priya-14 <kavipriyajk14@gmail.com>
2025-08-06 14:42:23 +0530

    Initial commit

--- Week-4/springresthands-on/exercise-2-load-date-bean/src/main/java/com/cognizant/springlearn/springlearnapplication.java
+++ Week-4/springresthands-on/exercise-2-load-date-bean/src/main/java/com/cognizant/springlearn/springlearnapplication.java
@@ -1,36 +1,18 @@
 package com.cognizant.springlearn;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.SpringApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 @SpringBootApplication
 public class SpringLearnApplication {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         LOGGER.info("START: SpringLearnApplication");
         SpringApplication.run(SpringLearnApplication.class, args);
-        displayDate();
         LOGGER.info("END: SpringLearnApplication");
-    }
-
-    public static void displayDate() throws Exception {
-        LOGGER.info("START: displayDate");
-
-        ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
-        SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
-
-        Date parsedDate = format.parse("31/12/2018");
-        LOGGER.debug("Parsed Date: {}", parsedDate.toString());
-
-        LOGGER.info("END: displayDate");
     }
 }
